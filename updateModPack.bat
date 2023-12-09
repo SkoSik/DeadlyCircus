1190157281c8c07ef3538bb0775e6edb579c8bce
SkoSik <skos2002d@gmail.com>
2023-12-09 15:50:12 +0800

    update updateModPack

--- updateModPack.bat
+++ updateModPack.bat
@@ -1,6 +1,5 @@
 @RD /S /Q "./BepInEx"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SkoSik/DeadlyCompany/archive/refs/heads/main.zip', 'modpack.zip')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SkoSik/DeadlyCompany/releases/download/Release/LethalCompany.zip', 'modpack.zip')"
 powershell -command "Expand-Archive modpack.zip ."
-XCOPY /S/Q/Y DeadlyCompany-main .
-@del ".\modpack.zip"
+del ".\modpack.zip"
 @RD /S /Q "./DeadlyCompany-main"
